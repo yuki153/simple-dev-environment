f339fd2f4b7b5017a420588c7932e45406662b8a
fujino-yuki <xxx153yuu.story@gmail.com>
2019-07-18 21:33:36 +0900

    Delete minimize property from css-loader.
    because css-loader update.

--- webpack.config.js
+++ webpack.config.js
@@ -47,8 +47,6 @@ module.exports = {
                   url: false,
                   // ソースマップの利用有無
                   sourceMap: true,
-                  // 空白文字やコメントを削除する
-                  minimize: false,
                   // Sass+PostCSSの場合は2を指定
                   importLoaders: 2
                 },
